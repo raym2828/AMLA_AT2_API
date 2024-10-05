914ab229e58e7b4166ba84e593e2dfaa26b63912
raym2828 <raytechie@gmail.com>
2024-10-05 16:33:04 +1000

    updated errro for forecasting

--- streamlit/app/pages/forecasting.py
+++ streamlit/app/pages/forecasting.py
@@ -7,13 +7,15 @@ if date:
         date_str = date[0].strftime('%Y-%m-%d')
     else:
         date_str = date.strftime('%Y-%m-%d')
-
-    try:
         response = requests.get(f"https://fast-api-1-0.onrender.com/sales/national?target_date={date_str}")
-        response.raise_for_status() 
-    except requests.exceptions.HTTPError as e:
-        st.write(f'Error: {e}')
-        st.stop()
+        response.raise_for_status()  # Raise an error for bad status codes
+        if "error" in response:
+            st.error(response["error"])
+        else:
+            forecast = response.json()
+            st.write(forecast)
+        
     
-    forecast = response.json()
-    st.write(forecast)
+    
+    # forecast = response.json()
+    # st.write(forecast)
