58648144a76c204b5399d1377bf09f775ebe8985
raym2828 <raytechie@gmail.com>
2024-10-05 16:47:08 +1000

    removed error

--- streamlit/app/pages/forecasting.py
+++ streamlit/app/pages/forecasting.py
@@ -8,14 +8,6 @@ if date:
     else:
         date_str = date.strftime('%Y-%m-%d')
         response = requests.get(f"https://fast-api-1-0.onrender.com/sales/national?target_date={date_str}")
-        response.raise_for_status()  # Raise an error for bad status codes
-        if "error" in response:
-            st.error(response["error"])
-        else:
-            forecast = response.json()
-            st.write(forecast)
-        
-    
-    
-    # forecast = response.json()
-    # st.write(forecast)
+     
+    forecast = response.json()
+    st.write(forecast)
